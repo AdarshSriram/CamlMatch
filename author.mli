f9cc20b7d781346a3ef325227cf8e94f4a160c82
Adarsh <as2566@cornell.edu>
2020-10-18 01:09:07 +0530

    Added Author CU

---
+++ author.mli
@@ -0,0 +1,6 @@
+(** CS 3110 Fall 2020 Final Project
+    @authors Rucha Gharpure (rpg75), Renee Shen (rbs288), Adarsh Sriram (as2566) 
+*)
+
+(** [hours_worked] on this assignment. *)
+val hours_worked : int
